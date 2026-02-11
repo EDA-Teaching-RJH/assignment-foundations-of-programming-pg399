b542c400c7c47fa78076cfac4a9f225100ecfbd7
pg399 <pg399@kent.ac.uk>
2026-02-11 20:43:42 +0000

    start_database function

---
+++ fleet_manager.py
@@ -0,0 +1,9 @@
+def start_database() :
+    names = ["Picard", "Riker","Data", "Wolf", "Troy" ]
+    ranks = ["Captain", "Commander","Lt Commander", "Lieutant", "Commander"]
+    divs  = ["Command", "Command", "Operations","Security", "Medical"]
+    ids   = ["001","002", "003", "004", "005"]
+
+    print("Crew data loaded successfully.")
+    return names, ranks,divs, ids
+          
