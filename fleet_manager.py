2e57d672ed98e83c6d01f13cc925d574e245091c
pg399 <pg399@kent.ac.uk>
2026-02-11 21:43:14 +0000

    task 2 cpmpleted

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,9 +1,38 @@
 def start_database() :
     names = ["Picard", "Riker","Data", "Wolf", "Troy" ]
-    ranks = ["Captain", "Commander","Lt Commander", "Lieutant", "Commander"]
-    divs  = ["Command", "Command", "Operations","Security", "Medical"]
+    ranks = ["Rank 1", "Rank 2","Rank 3", "Rank 4", "Rank 5"]
+    divs  = ["Division A", "Division B", "Division C","Division D", "Division E"]
     ids   = ["001","002", "003", "004", "005"]
 
-    print("Crew data loaded successfully.")
+    print("Database Intialized.")
     return names, ranks,divs, ids
-          
+
+def display_menu(student_name):
+    print(f"\n--- Fleet Manager: {student_name} ---")
+    print("1. View Crew Roster")
+    print("2. Add Member")
+    print("3. Exit")
+
+    choice = input ("Select an option: ")
+    return choice
+
+def main():
+    
+    names, ranks, divs, ids = start_database()
+    my_name = "Peter"
+
+    active = True
+    while active:
+        user_choice = display_menu(my_name)
+        if user_choice == "1":
+            print("\nViewing Crew Roster...(Function coming next)")
+        elif user_choice == "2" :
+            print("\nAdding New Member...(Function coming next)")
+        elif user_choice =="3" :
+            print("Exiting program.")
+            active = False
+        else:
+            print("Invalid input, please try again.")
+if __name__ == "__main__":
+    main()
+ 
